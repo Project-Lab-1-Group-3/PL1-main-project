f1b3cf5e07368846dd0e4a556ba3a4e6cccd040b
johnkramer0 <98244022+johnkramer0@users.noreply.github.com>
2022-03-21 22:15:38 -0500

    Midterm Update

--- Motor_Driver.v
+++ Motor_Driver.v
@@ -7,19 +7,19 @@ module Motor_Driver(
     input 	[7:0] JB,     		//phototransistor and IPS inputs
     output 	[7:0] JA,    		//JA Pmod port for output to H-bridge
     output 	[3:0] an,    		//SSD anode
-    output 	[6:0] seg, 		  	//SSD segments
+    output 	[6:0] seg, 		//SSD segments
     output 	[7:0] led,  		//LEDs for debugging
     output 	dp           		//decimal point is always off
 );
-	reg [7:0] 	JA_temp; 			//Used for output to the motors
-	reg [6:0] 	seg_temp;			//Used for seven segment display output
+	reg [7:0] 	JA_temp; 		//Used for output to the motors
+	reg [6:0] 	seg_temp;		//Used for seven segment display output
 	reg [1:0] 	JC_old = 2'b11;		//Remembers the previous JC input value
 	reg [20:0] 	counter60 = 0;		//60 Hz counter for the motors
 	reg [10:0] 	pulse_counter0, pulse_counter1, pulse_counter2, pulse_counter3;	//Count the number of pulses per 100 ms
 	reg [23:0] 	pulse_timer = 0;	//10 Hz counter for the phototransistors
 	reg [3:0] 	freq, old_pulse, last_direction;		
 	reg 		stop = 0; 			//for current limiting
-	parameter	F = 4'b0011, 		//Arbitrary numbers assigned to F and C parameters
+	parameter	F = 4'b0011, 			//Arbitrary numbers assigned to F and C parameters
 				C = 4'b1100,
 				FORWARD = 4'b1010, 	//parameters correspond to necesary JA outputs to move the motors
 				BACK    = 4'b0101,
@@ -35,9 +35,9 @@ module Motor_Driver(
 		else counter60 = 0; //reset counter every 1,666,666 clock cycles
 		
 		//10 Hz counter to check frequency of phototransistor input
-		if (pulse_timer < 10000000) begin              	//executes every clock cycle
+		if (pulse_timer < 10000000) begin              		//executes every clock cycle
 			pulse_timer = pulse_timer + 1;
-			if (old_pulse[0] != JB[0]) 					//if phototramsistor input has changed
+			if (old_pulse[0] != JB[0]) 			//if phototramsistor input has changed
 			     pulse_counter0 = pulse_counter0 + 1;  	//increment appropriate pulse counter
 			if (old_pulse[1] != JB[1]) 		
 			     pulse_counter1 = pulse_counter1 + 1;
@@ -97,23 +97,23 @@ module Motor_Driver(
 		          last_direction = RIGHT;
 		      end
 		  end else if (JB[7:4] == 4'b1111) begin 	//no sensors are active (path is lost)
-		      case (last_direction) 				//Continue turning in same direction as previous turn 
+		      case (last_direction) 			//Continue turning in same direction as previous turn 
 		          RIGHT: JA_temp = P_RIGHT; 		//switch to a pivot instead of a gradual turn
 		          LEFT: JA_temp = P_LEFT;
         	   endcase
         	end else JA_temp = FORWARD;  			//go forward if no cases are true
-		end else JA_temp = 0; 						//motors are off when stop = 1 or counter60 > SPEED
+		end else JA_temp = 0; 				//motors are off when stop = 1 or counter60 > SPEED
 		
 		//if statement stops the motors if current limiter is active for 100 ms
 		if (JC != 2'b11 && JC_old != 3'b11) stop = 1; 
 	end
 	
-	//Assigning outputs
-    assign led[3:0] = JB[7:4];	//LEDs are used for debugging
+    //Assigning outputs
+    assign led[3:0] = JB[7:4];		//LEDs are used for debugging
     assign led[5:4] = JC[1:0];
     assign led[6] = stop;
 	assign an = 4'b1110; 		//only the first digit of the seven segment display is used for now
-	assign dp = 1;				//decimal point is always off
+	assign dp = 1;			//decimal point is always off
 	assign JA = JA_temp;		//temp registers are used in the procedural code and assigned to outputs
 	assign seg = seg_temp;
 endmodule
